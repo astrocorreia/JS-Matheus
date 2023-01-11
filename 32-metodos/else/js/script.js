3be5cfc73e78d261350cef622ff34ab678a2b062
Astrogildo Correia <108525041+astrocorreia@users.noreply.github.com>
2023-01-11 11:34:33 +0000

    Métodos

---
+++ 32-metodos/else/js/script.js
@@ -0,0 +1,10 @@
+const cachorro = {
+    uivar: function() {
+        console.log("Auuuuuuuu");
+    },
+    rosnar: function () {
+        console.log("Grrrrrrr");
+    }
+}
+cachorro.uivar();
+cachorro.rosnar();
