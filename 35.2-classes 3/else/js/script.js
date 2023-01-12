74779cfcd6d8c56b85a646a99a33c3efe5aed426
Astrogildo Correia <108525041+astrocorreia@users.noreply.github.com>
2023-01-12 10:40:34 +0000

    Construtor New

---
+++ 35.2-classes 3/else/js/script.js
@@ -0,0 +1,16 @@
+function Cachorro(raca, patas, cor) {
+
+    
+    this.raca = raca;
+    this.patas = patas;
+    this.cor = cor;
+    this.latir = function() {
+        console.log("Au au");
+    }
+    
+}
+
+
+let husky =  new Cachorro("Husky", 4, "Preta");
+
+husky.latir();
