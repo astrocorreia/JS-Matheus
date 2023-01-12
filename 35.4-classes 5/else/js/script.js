6a1b0d9278f4d052f036322301d42cabf8ff5e16
Astrogildo Correia <108525041+astrocorreia@users.noreply.github.com>
2023-01-12 11:16:22 +0000

    Construtor na Classe ES6

---
+++ 35.4-classes 5/else/js/script.js
@@ -0,0 +1,15 @@
+
+
+
+class Cachorro {
+    constructor(raca, patas, cor) {
+    this.raca = raca;
+    this.patas = patas;
+    this.cor = cor;
+    }
+    
+}
+
+let labrador =  new Cachorro("Labrador", 4, "Preta");
+
+console.log(labrador);
