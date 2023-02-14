259726511de5c08dc11208e8d925dce567b0e394
Astrogildo Correia <108525041+astrocorreia@users.noreply.github.com>
2023-02-14 15:44:54 +0000

    Exercícios

---
+++ 59-Exercício variados copy/js/script.js
@@ -0,0 +1,26 @@
+console.log(33);
+console.log(15.7);
+console.log(+18.9);
+
+console.log(typeof 33);
+console.log(typeof 15.7);
+console.log(typeof+18.9);
+
+console.log(12 + 8);
+console.log(typeof (12 + 8));
+
+console.log (42 * 3);
+console.log(typeof(42 * 3));
+
+let nome = "Astro";
+let idade = 48;
+let cnh = true;
+
+console.log(typeof nome);
+console.log(typeof idade);
+console.log(typeof cnh);
+
+console.log(50+07);
+console.log(12%3);
+console.log(typeof (50+07));
+console.log(typeof "12%3");
