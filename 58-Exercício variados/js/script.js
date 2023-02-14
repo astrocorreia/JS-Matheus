cbcc40f30d1ad001ba9cc60e2bad2d9a2e5287ab
Astrogildo Correia <108525041+astrocorreia@users.noreply.github.com>
2023-02-14 15:22:33 +0000

    Exercícios

---
+++ 58-Exercício variados/js/script.js
@@ -0,0 +1,26 @@
+var nome = "Astro";
+console.log(nome);
+
+var numero = 3;
+console.log(numero);
+
+var soma = 3; 5; 7;
+console.log(3+5+7);
+
+var num = 1; 8; 6;
+console.log(1 + 8 * 6)
+
+var limao = 4;
+console.log(limao * limao);
+
+let carro = "Onibus";
+console.log(carro);
+
+carro = "carro";
+console.log(carro);
+
+astro = "Pedro";
+console.log(astro);
+
+var num = 89; 33; 12;
+console.log(89 - 33 + 12 );
